7b35755466866591b3282cba6eaa1923c69a46fc
Jere Lantz <lantz.jere@gmail.com>
2025-04-11 21:35:28 +0300

    added a small wrapper component

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
+import userEvent from "@testing-library/user-event"
 
 describe("Greeting component", ()=>{
     test("Renders Hello world", ()=>{
@@ -12,5 +13,39 @@ describe("Greeting component", ()=>{
         // Assert
         const elem = screen.getByText("Hello World")
         expect(elem).toBeInTheDocument()
+    })
+
+    test(`Renders "It's good to see you" if button was not pressed`,()=>{
+        render(<Greeting />)
+
+        const elem = screen.getByText("It's good to see you", {exact:false})
+        expect(elem).toBeInTheDocument()
+    })
+
+    test("Renders 'changed!' if button was pressed", async()=>{
+        // Arrange
+        render(<Greeting />)
+
+        // Act
+        const btnElem = screen.getByRole("button")
+        await userEvent.click(btnElem)
+
+        // Assert
+        const elem = screen.getByText("Changed!", {exact:false})
+        expect(elem).toBeInTheDocument()
+    })
+
+    test("Original text doesn't exist after the button is pressed", async() =>{
+        // Arrange
+        render(<Greeting />)
+
+        // Act
+        const btnElem = screen.getByRole("button")
+        await userEvent.click(btnElem)
+
+        // Assert
+        const elem = screen.queryByText("It's good to see you", {exact:false})
+        //expect(elem).not.toBeInTheDocument() tai
+        expect(elem).toBeNull()
     })
 })
